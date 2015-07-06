c8ae4b9bc00f4e66e69a03eb4a383409d7625c5c
rokade <rupakrokade@gmail.com>
2015-07-06 18:23:47 +0530

    Multiplication code completed

--- multiply.c
+++ multiply.c
@@ -1,17 +1,21 @@
 /*This c program multiplies two variable values*/
 #include<stdio.h>
-double multi(double a,double b)
-{
- /*printf("%f\n", a);
- printf("%f\n", b);*/
- double ret;
- ret=a*b;
- return ret;
-}
 
-double main(double argv[])
+float main(void)
 {
- multi(argv[2],argv[3]);
+ float num1, num2, result; /*Type declaration of variables*/
+  
+ printf("Enter first number to multiply: ");
+ scanf("%f", &num1);
+ 
+ printf("Enter second number to multiply: ");
+ scanf("%f", &num2);
+
+ result=num1*num2;
+
+ printf("The multiplication result is %f \n", result);
+
+ return 0;
 }
 
 
